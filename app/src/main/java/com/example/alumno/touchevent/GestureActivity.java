48c438411bf98b29d69e4213679ae7eae40bad44
Alejandro7360 <ichigo_11_619@live.com.mx>
2017-08-24 21:08:16 -0500

    cualquier mensaje

--- app/src/main/java/com/example/alumno/touchevent/GestureActivity.java
+++ app/src/main/java/com/example/alumno/touchevent/GestureActivity.java
@@ -1,11 +1,17 @@
 package com.example.alumno.touchevent;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
 public class GestureActivity extends AppCompatActivity implements
@@ -32,6 +38,9 @@ public class GestureActivity extends AppCompatActivity implements
 
     @Override
     public boolean onDown(MotionEvent event) {
+        changeBackgroundColor(Color.GRAY);
+        TextView textView = (TextView) findViewById(R.id.hello_Gesture);
+        textView.setText("El Down");
         Log.d(DEBUG_TAG,"onDown: " + event.toString());
         return true;
     }
@@ -39,14 +48,18 @@ public class GestureActivity extends AppCompatActivity implements
     @Override
     public boolean onFling(MotionEvent event1, MotionEvent event2,
                            float velocityX, float velocityY) {
+        changeBackgroundColor(Color.GREEN);
+        TextView textView = (TextView) findViewById(R.id.hello_Gesture);
+        textView.setText("aceptada ;v");
         Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
         return true;
     }
 
     @Override
     public void onLongPress(MotionEvent event) {
+        changeBackgroundColor(Color.RED);
         TextView textView = (TextView) findViewById(R.id.hello_Gesture);
-        textView.setText("Long Press Done");
+        textView.setText("Long Press Done ( ͡° ͜ʖ ͡°)");
         Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
     }
 
@@ -76,13 +89,39 @@ public class GestureActivity extends AppCompatActivity implements
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent event) {
+        changeBackgroundColor(Color.BLUE);
+        TextView textView = (TextView) findViewById(R.id.hello_Gesture);
+        textView.setText("dat spray :v");
         Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
         return true;
     }
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent event) {
+        changeBackgroundColor(Color.MAGENTA);
+        TextView textView = (TextView) findViewById(R.id.hello_Gesture);
+        textView.setText("One Tap ;v");
         Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
         return true;
+    }
+    public void changeBackgroundColor(int color) {
+        View view  = findViewById(R.id.main_Layout);
+        Drawable background = view.getBackground();
+        int colorFrom = Color.TRANSPARENT;
+        if (background instanceof ColorDrawable)
+            colorFrom = ((ColorDrawable) background).getColor();
+        int colorTo = color;
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(250); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                View view  = findViewById(R.id.main_Layout);
+                view.setBackgroundColor((int) animator.getAnimatedValue());
+            }
+
+        });
+        colorAnimation.start();
     }
 }
